10e191f7301ea698979475d1b4bcbec475e29609
Emmanuel C <misterium273@gmail.com>
2023-04-20 19:14:07 -0400

    style: se añadieron archivos tipo page para comprobar rutas

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-red-200">HOME</div>
+      <div className="bg-red-200">this is the principal page</div>
     </>
   );
 }
