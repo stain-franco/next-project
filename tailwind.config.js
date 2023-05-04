27cdf2399d40952e4f46568062b97ba3218d7094
franco <sebas18pet@gmail.com>
2023-05-03 19:20:00 -0500

    fix: se resuelven problemas con vercel 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
   ],
   // Ensure these match with .storybook/preview.js
   theme: {
-<<<<<<< HEAD
 		extend: {
 		  colors: {
 			  'White': '#ffffff',
@@ -43,27 +42,6 @@ module.exports = {
       Mulish:["Mulish","sans-serif"]
     }
     
-=======
-    extend: {
-      colors: {
-        White: '#fffff',
-        backgroud: '#E6ECF5',
-        DarkBlue: '#07469C',
-        blue: '#0099E1',
-        Coral: '#EF3061',
-        DarkGrey: '#333333',
-        Grey: '#757575',
-        Ligthgray: '#F2F2F2',
-      },
-      screens: {
-        xs: '375px',
-        sm: '600px',
-        md: '900px',
-        lg: '1200px',
-        xl: '1536px',
-      },
-    },
-    plugins: [],
->>>>>>> main
   },
-};
+}
+}
