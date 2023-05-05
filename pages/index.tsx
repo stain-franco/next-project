682d9f9ce81bd89affa75dc6a482c86ec33e8583
franco <sebas18pet@gmail.com>
2023-05-04 20:27:48 -0500

    fix: se cambia nombre de archivo foto para poder visualizar 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -72,7 +72,7 @@ const Page: NextPageWithLayout = () => { //
         <section className="xs:px-3 xs:py-28 lg:py-52 xl:px-24 bg-white xs:-skew-y-12 xl:-skew-y-6">
           <div className="flex justify-evenly items-center flex-wrap gap-10 xs:skew-y-6 xl:skew-y-3">
             <Image
-              src={photo}
+              src="/images/Group 25.png"
               alt="Picture of the author"
               className="xs:min-w-min   lg:max-w-lg "
               width={405}
